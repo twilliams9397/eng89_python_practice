cef7c3af38bf2440b0bb1dddf2a9778a7af037dc
Tom <Tom1@MacBook-Pro-2.home>
2021-07-01 10:24:36 +0100

    Question 1 and 2

---
+++ practice.py
@@ -0,0 +1,14 @@
+# declare a function called Username, that takes one arg as str, and return the name
+
+# def username(name):
+#     return name
+#
+# name_input = input("What is your name?  ")
+#
+# print(username(name_input))
+
+# declare a list with numbers 1 to 5. iterate through the list and display the list
+
+num_list = [1, 2, 3, 4, 5]
+for i in num_list:
+    print(i)
